5367cfa0dcd7a50f3375afb7d3b8359dc50be5f6
Steve Sharp <sharps.no.reply@fastmail.com>
2018-12-14 10:02:22 -0500

    Initial commit

---
+++ init.el
@@ -0,0 +1,28 @@
+(package-initialize)
+
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
+;; Set path to dependencies
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+
+(setq settings-dir
+      (expand-file-name "settings" user-emacs-directory))
+
+;; Set up load path
+(add-to-list 'load-path settings-dir)
+(add-to-list 'load-path site-lisp-dir)
+
+;; Keep emacs Custom-settings in separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
+;; Use better-defaults ( https://github.com/technomancy/better-defaults )
+(add-to-list 'load-path "/path/to/better-defaults")
+(require 'better-defaults)
+
+;; Settings for currently logged in user
+(setq user-settings-dir
+      (concat user-emacs-directory "users/" user-login-name))
+(add-to-list 'load-path user-settings-dir)
